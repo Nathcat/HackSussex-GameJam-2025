14956f6e7d74bb176bf79abd361ac4cd50e6417a
Brooke <46959407+brooke-ec@users.noreply.github.com>
2025-12-06 20:36:56 +0000

    player sprite

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -7,15 +7,36 @@ public class Entity : MonoBehaviour
     [field: SerializeField] public GUID Guid { get; private set; }
     [field: SerializeField] public float Health { get; private set; }
     [SerializeField] public float MaxHeath;
-    public UnityEvent onHeathChange = new UnityEvent();
+    [SerializeField] private float animationSpeed = 0.5f;
 
-    void Start()
+    public readonly UnityEvent onHeathChange = new UnityEvent();
+
+    private SpriteRenderer sprite;
+    private float animation;
+    private Vector2 old;
+
+    protected virtual void Start()
     {
         Guid = GUID.Generate();
+        old = transform.position;
+        sprite = transform.Find("Sprite").GetComponent<SpriteRenderer>();
     }
 
-    void Update()
+    protected virtual void Update()
     {
-        
+        Vector2 position = transform.position;
+        Vector2 delta = position - old;
+
+        if (delta.x < 0) sprite.flipX = true;
+        if (delta.x > 0) sprite.flipX = false;
+
+        if (animation > 0)
+        {
+            sprite.transform.localPosition = new Vector2(0, Mathf.Sin(animation) * 0.5f);
+            sprite.transform.localScale = new Vector2(12, 12 - Mathf.Cos(animation * 2));
+            animation -= Time.deltaTime * animationSpeed;
+        } else if (delta != Vector2.zero) animation = Mathf.PI;
+
+        old = position;
     }
 }
