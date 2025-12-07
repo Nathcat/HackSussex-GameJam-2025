715f1073ec1c19807e303a9d315ac7686b2da36d
Brooke <46959407+brooke-ec@users.noreply.github.com>
2025-12-07 06:22:54 +0000

    passive mana regen

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : Entity
 
     [field: SerializeField] public float mana { get; private set; }
     [SerializeField] public float maxMana;
+    [SerializeField] private float manaRegen;
     public readonly UnityEvent onManaChange = new UnityEvent();
 
     new private Rigidbody2D rigidbody;
@@ -30,6 +31,7 @@ public class PlayerController : Entity
         Vector2 velocity = moveAction.ReadValue<Vector2>() * walkSpeed;
         if (Application.isEditor && sprintAction.IsPressed()) velocity *= 5;
         rigidbody.linearVelocity = velocity;
+        AddMana(manaRegen * Time.deltaTime);
     }
 
     public void OnDeath() {
