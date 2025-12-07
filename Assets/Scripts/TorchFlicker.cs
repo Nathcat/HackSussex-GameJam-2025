aad35ac43b51203b5276e39aa7c3fe42b7350d8e
Brooke <46959407+brooke-ec@users.noreply.github.com>
2025-12-07 11:22:50 +0000

    torch flicker

---
+++ Assets/Scripts/TorchFlicker.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class TorchFlicker : MonoBehaviour
+{
+    private Light2D light;
+    private float target;
+    private float initial;
+    private float speed;
+    
+    void Start()
+    {
+        light = GetComponent<Light2D>();
+        initial = light.intensity;
+        Choose();
+    }
+
+    void Update()
+    {
+        light.intensity = Mathf.Lerp(light.intensity, target, speed);
+    }
+
+    void Choose()
+    {
+        speed = Random.Range(0.01f, 0.1f);
+        target = initial + Random.Range(-0.5f, 0.5f);
+        this.RunAfter(Random.Range(0.2f, 0.8f), Choose);
+    }
+}
