f049dc835247bb05c7bc8118bbdec68a39f18a4c
Brooke <46959407+brooke-ec@users.noreply.github.com>
2025-12-07 05:01:41 +0000

    spell door!

---
+++ Assets/Scripts/SpellDoor.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SpellDoor : MonoBehaviour
+{
+    [SerializeField] private Spell required;
+    [SerializeField] private float distance = 3;
+
+    private Transform diagram;
+
+    void Start()
+    {
+        diagram = transform.GetChild(0);
+        SpellEventManager.instance.onSpellCast.AddListener(CheckSpell);
+    }
+
+    private void Update()
+    {
+        diagram.localPosition = new Vector2(0, Mathf.Sin(Time.realtimeSinceStartup) * 0.02f);
+    }
+
+    private void CheckSpell(Spell spell, Vector3 _, Vector3 target)
+    {
+        if (spell == required && Vector2.Distance(diagram.position, target) <= distance)
+        {
+            ParticlePlayer.Activate(transform.GetChild(1).gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
