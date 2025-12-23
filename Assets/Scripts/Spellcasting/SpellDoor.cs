4db49f556920e7b1b1d25d449f5e8c75c9e63f3b
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-12-23 18:35:20 +0000

    fix: spell door error getting stuck

--- Assets/Scripts/Spellcasting/SpellDoor.cs
+++ Assets/Scripts/Spellcasting/SpellDoor.cs
@@ -6,12 +6,15 @@ public class SpellDoor : SpellHint
     [SerializeField] private Sprite error;
 
     private SpriteRenderer rune;
+    private float timer = 0;
+    private Sprite initial;
 
     void Start()
     {
         SpellEventManager.instance.onSpellCast.AddListener(CheckSpell);
         SpellEventManager.instance.onSpellFailed.AddListener((_) => { if (inRange) Failed(); });
         rune = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        initial = rune.sprite;
     }
 
     private void CheckSpell(Spell spell, Vector3 _, Vector3 target)
@@ -34,8 +37,16 @@ public class SpellDoor : SpellHint
     private void Failed()
     {
         AudioManager.instance.doorDeny.PlayAt(transform.position);
-        Sprite old = rune.sprite;
         rune.sprite = error;
-        this.RunAfter(2, () => { rune.sprite = old; });
+        timer = 2;
+    }
+
+    private void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0) rune.sprite = initial;
+        }
     }
 }
