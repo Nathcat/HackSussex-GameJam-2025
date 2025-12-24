a53d0d387cad95a5017e52c3ccfb43a3cd98c565
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-12-24 23:01:50 +0000

    feat: improve game over screen

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [DefaultExecutionOrder(-100)]
 public class Music : MonoBehaviour
 {
-    [SerializeField] private AudioClip baseMusic;
-    [SerializeField] private AudioClip combatOverlay;
+    [SerializeField] private AudioClip baseClip;
+    [SerializeField] private AudioClip melodyClip;
+    [SerializeField] private AudioClip combatClip;
     [SerializeField][Range(0, 1)] private float volume = 0.126f;
 
     public bool muted = false;
 
-    private AudioSource overlaySource;
+    private AudioSource combatSource;
+    private AudioSource melodySource;
     private AudioSource baseSource;
     private bool inCombat = false;
+
+    private bool gameOver
+    {
+        get
+        {
+            return SceneManager.GetActiveScene().name == "GameOver";
+        }
+    }
 
     public static Music instance { get; private set; } = null;
 
@@ -26,23 +37,32 @@ public class Music : MonoBehaviour
     {
         baseSource = gameObject.AddComponent<AudioSource>();
         baseSource.bypassListenerEffects = true;
-        baseSource.clip = baseMusic;
+        baseSource.clip = baseClip;
+        baseSource.volume = volume;
         baseSource.loop = true;
 
-        overlaySource = gameObject.AddComponent<AudioSource>();
-        overlaySource.bypassListenerEffects = true;
-        overlaySource.clip = combatOverlay;
-        overlaySource.loop = true;
-        overlaySource.volume = 0;
+        melodySource = gameObject.AddComponent<AudioSource>();
+        melodySource.bypassListenerEffects = true;
+        melodySource.clip = melodyClip;
+        melodySource.volume = volume;
+        melodySource.loop = true;
+
+        combatSource = gameObject.AddComponent<AudioSource>();
+        combatSource.bypassListenerEffects = true;
+        combatSource.clip = combatClip;
+        combatSource.loop = true;
+        combatSource.volume = 0;
 
         baseSource.Play();
-        overlaySource.Play();
+        melodySource.Play();
+        combatSource.Play();
     }
 
     void Update()
     {
-        baseSource.volume = muted ? 0 : volume;
-        overlaySource.volume = Mathf.MoveTowards(overlaySource.volume, inCombat && !muted ? volume : 0, Time.deltaTime);
+        baseSource.volume = Mathf.Lerp(baseSource.volume, muted ? 0 : volume, Time.deltaTime * 2);
+        melodySource.volume = Mathf.Lerp(melodySource.volume, muted || gameOver ? 0 : volume, Time.deltaTime * 2);
+        combatSource.volume = Mathf.Lerp(combatSource.volume, ((inCombat || gameOver) && !muted) ? volume : 0, Time.deltaTime * 2);
         inCombat = false;
     }
 
